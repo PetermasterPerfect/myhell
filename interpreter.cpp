5755f83ac3836b5e7132b65c4e26647ed4f25259
petermasterperfect@gmail.com <debian@alamakota.home>
2023-12-13 21:14:29 +0100

    split cmd by operators such as <,>,|

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,19 +1,78 @@
 #include "interpreter.h"
 
+void atomic_cmd::reset()
+{
+	argv.clear();
+	redirect_in.clear();
+	redirect_out.clear();
+}
+
 Interpreter::Interpreter()
 {
 	cwd = fs::current_path();
 }
 
-void Interpreter::set_finish(int st, std::string in)
+void Interpreter::set_finish_stat(int st, std::string in)
 {
 	finish.status = st;
 	finish.info = in;
 }
 
-void Interpreter::order_arguments()
+void Interpreter::order_arguments(std::vector<atomic_argument*> original_cmd)
 {
+	std::vector<atomic_cmd> finish;
+	bool out = false;
+	atomic_cmd buf;
+	auto ref = &buf.argv;
+	for(auto &i : original_cmd)
+	{
+		if(i->operand)
+		{
+			if(out)
+			{
+				buf.redirect_out = i->str;
+				out = false;
+			}
+			else
+				ref->push_back(i->str);
+		}
+		else
+		{
+			switch(i->str.c_str()[0]) // when we deal with operator we're sure str is 1 byte long
+			{
+				case '>':
+					out = true;
+					break;
+				case '<':
+					ref = &buf.redirect_in;
+					break;
+				case '|':
+					finish.push_back(buf);
+					buf.reset();
+					ref = &buf.argv;
+			}
+		}
+	}
+	finish.push_back(buf);
+	ordered_cmd = finish;
+}
 
+
+void Interpreter::print_ordered_cmd()
+{
+	for(auto &s: ordered_cmd)
+	{
+		std::cout << "argv: ";
+		for(auto &a: s.argv)
+			std::cout << a << ", ";
+
+		std::cout << "\nin: ";
+		for(auto &i: s.redirect_in)
+			std::cout << i << ", ";
+
+		std::cout << "\nout: ";
+		std::cout << s.redirect_out << "\n";
+	}
 }
 
 void Interpreter::run_process(std::string path, std::vector<std::string> arg)
@@ -33,7 +92,7 @@ void Interpreter::run_process(std::string path, std::vector<std::string> arg)
 	else 
 	{
 		// we are the child
-		execve(...);
+		//execve(...);
 		_exit(EXIT_FAILURE);   // exec never returns
 	}
 }
--- interpreter.h
+++ interpreter.h
@@ -1,38 +1,44 @@
 #include <vector>
 #include <string>
 #include <filesystem>
+#include <functional>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "string_logic.h"
 
 namespace fs = std::filesystem;
 
 struct finish_status
 {
-	int status
+	int status;
 	std::string info;
-}
+};
 
-struct atom_cmd
+struct atomic_cmd
 {
-	std::string argv;
-	std::string redirect_in;
+	std::vector<std::string> argv; // argv[0] - executable file path
+	std::vector<std::string> redirect_in;
 	std::string redirect_out;
-}
+	bool in, out;
+	void reset();
+};
 
 class Interpreter
 {
 	fs::path cwd;
-	std::vector<atomic_argument> org_cmd;
-	std::vector<std::string> ordered_cmd;
+	std::vector<atomic_cmd> ordered_cmd;
 	
 	finish_status finish;
 	
-	Interpreter();
-	~Interpreter();
 
-	void order_arguments();
-	void set_cmd(std::vector<std::string>);
 	void run_process(std::string, std::vector<std::string>);
 	void execute();
 
-	void set_finish(int, std::string);
+	void set_finish_stat(int, std::string);
+
+public:
+	void print_ordered_cmd();
+	void order_arguments(std::vector<atomic_argument*>);
+	Interpreter();
 };
