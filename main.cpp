ccc494e474ab57bee10426c228a33d23e73da991
petermasterperfect@gmail.com <petermasterperfect@gmail.com>
2024-10-24 20:09:32 +0200

    Set up antlr4 with cmake and grammar for scripting language

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
-#include "interpreter.h"
+#include "antlr4-runtime.h"
+#include "HadesParser.h"
+#include "HadesLexer.h"
+#include "HadesParserBaseVisitor.h"
 #include <iostream>
 
-int main()
+using namespace antlr4;
+using namespace antlr4::tree;
+
+class TestVisitor : public HadesParserBaseVisitor
 {
 
-	while(1)
+};
+
+
+int main(int argc, char **argv)
+{
+	//while(1)
 	{
-		std::string s = read_command();
-		//std::cout << "----------" << std::endl;
-		//std::cout << s;
-		//std::cout << "----------\n" << std::endl;
-		auto v = split_cmd(s);
-		Interpreter interp;
-		interp.order_arguments(v);
-		//interp.print_present_cmd();
-		interp.execute_cmd();
-		//for(int i=0; i<v.size(); i++)
-		//	std::cout << "i=" << i << " " << v[i]->str << " , " << v[i]->operand << "\n";
-		free_args(v);
+		std::ifstream f("../test.had");
+		ANTLRInputStream input(f);
+		//ANTLRInputStream input(std::string("x=sada"));
+		HadesLexer lexer(&input);
+		CommonTokenStream tokens(&lexer);
+		HadesParser parser(&tokens);
+		
+		ParseTree *tree = parser.program();
+		TestVisitor test;
+		test.visit(tree);
 	}
 }
